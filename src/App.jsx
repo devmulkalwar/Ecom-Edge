06e02d931a2d733a005a633132c3792f8ce65fea
devmulkalwar <devmulkalwar95@gmail.com>
2024-02-11 19:56:56 +0530

    folder structure added

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='w-auto flex justify-center bg-gray-900 p-6 m-2 font-bold text-3xl text-red-400'>Hello World!!</h1>
+    </>
+  )
+}
+
+export default App
