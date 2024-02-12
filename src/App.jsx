acda0b2d991b510d16038c0c611e474fa90ca3df
Harsh Ninave <harshninave58@gmail.com>
2024-02-12 19:31:40 +0530

    Navbar visiblity

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import React from 'react'
-import Navbar from './Components/Navbar'
-
+// import React from 'react'
+import Navbar from "./Components/Navbar";
 
 function App() {
-
   return (
     <>
       {/* <h1 className='w-auto flex justify-center bg-gray-900 p-6 m-2 font-bold text-3xl text-red-400'>Hello World!!</h1> */}
-     
+      <Navbar />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
